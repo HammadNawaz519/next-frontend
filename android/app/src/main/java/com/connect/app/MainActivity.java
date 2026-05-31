092379a3c559113db963801de159081b4e1cb92c
Hammad <hammadnawaz519@gmail.com>
2026-05-31 05:11:36 +0500

    configure transparent status bar in MainActivity to enable immersive edge-to-edge layout

--- android/app/src/main/java/com/connect/app/MainActivity.java
+++ android/app/src/main/java/com/connect/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.connect.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Make status bar transparent and flow webview content edge-to-edge (takes the whole top bar of the phone)
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+        window.setStatusBarColor(Color.TRANSPARENT);
+    }
+}
