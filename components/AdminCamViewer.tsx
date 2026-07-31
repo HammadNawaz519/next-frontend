4f84ab8ef894d2f5f3cea1a21f22e974be4c2c34
HammadNawaz519 <hammadnawaz519@gmail.com>
2026-07-31 15:54:56 +0500

    fix: replace openrelay TURN with Metered.ca dedicated TURN servers for cross-network video streaming

--- components/AdminCamViewer.tsx
+++ components/AdminCamViewer.tsx
@@ -8,17 +8,27 @@ const ADMIN_EMAILS = ['hammadnawz519@gmail.com', 'hammadnawaz519@gmail.com'];
 
 const RTC_CONFIG: RTCConfiguration = {
   iceServers: [
+    { urls: 'stun:stun.relay.metered.ca:80' },
     {
-      urls: [
-        'stun:stun.l.google.com:19302',
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302',
-      ],
+      urls: 'turn:global.relay.metered.ca:80',
+      username: '3fe6f0a72ac7f100111cacfe',
+      credential: 'k8LmNASFj+JSwE0D',
     },
-    { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
-    { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
+    {
+      urls: 'turn:global.relay.metered.ca:80?transport=tcp',
+      username: '3fe6f0a72ac7f100111cacfe',
+      credential: 'k8LmNASFj+JSwE0D',
+    },
+    {
+      urls: 'turn:global.relay.metered.ca:443',
+      username: '3fe6f0a72ac7f100111cacfe',
+      credential: 'k8LmNASFj+JSwE0D',
+    },
+    {
+      urls: 'turns:global.relay.metered.ca:443?transport=tcp',
+      username: '3fe6f0a72ac7f100111cacfe',
+      credential: 'k8LmNASFj+JSwE0D',
+    },
   ],
   iceCandidatePoolSize: 10,
 };
